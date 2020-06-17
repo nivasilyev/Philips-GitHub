7946725fbcb8d64fcf7cadc2a1cd611053f64e6d
nivasilyev <65684866+nivasilyev@users.noreply.github.com>
2020-06-17 13:46:32 -0700

    Update indeedJobSearch.py

--- indeedJobSearch.py
+++ indeedJobSearch.py
@@ -1,23 +1,38 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jun  4 23:34:49 2020
-indeed Job Search
-using custom URLs
+Created on Sat Jun  6 19:11:56 2020
 
 @author: Rizilip
 """
-import webbrowser
-from formatCity import formatCity
 
-jobFile = open('E:\Programming\eclipseWorkspace\globeProject\globeProject.txt', encoding="utf8")
-cities = jobFile.readlines()
-jobFile.close()  
 
-for city in cities:
-    formatCity(city, "indeed")
-    url = "https://www.indeed.com/jobs?q=IT+help+desk&l=" + formatCity(city, "indeed")
+import webbrowser, random
+from formatString import formatString
+
+
+cityFile = open('E:\Programming\globeProject.txt', encoding="utf8")
+cities = cityFile.readlines()
+cityFile.close()  
+
+taskFile = open('E:\Programming\list0.txt', 'r')
+tasks = taskFile.readlines()
+taskFile.close()
+
+
+reply = ""
+Y= 0
+for lines in cities:
+    Y+=1
+    url = ""
+    searchTerm = tasks[random.randrange(20)]
+    url = "https://www.indeed.com/jobs?q=" + formatString(searchTerm, "indeedTask") + "+$30,000&l=" + formatString(lines, "indeedCity")
     webbrowser.open(url)
     
-    
-url = "https://keep.google.com/u/0/"
-webbrowser.open(url)
+    if Y%10 ==0:
+        reply = input("hey press [br] to break early.\n")
+        if reply == "br":
+            
+            break    
+if reply != "br":
+    url = "https://keep.google.com/u/0/"
+    webbrowser.open(url)
